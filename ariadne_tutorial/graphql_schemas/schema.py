5c52da9abc5880c3f32bbf3a771d63ed812f5a70
ZERO20 <edgargdcv@gmail.com>
2023-01-09 23:25:37 -0600

    ref: resolvers

--- ariadne_tutorial/graphql_schemas/schema.py
+++ ariadne_tutorial/graphql_schemas/schema.py
@@ -1,18 +1,23 @@
-from ariadne import QueryType, make_executable_schema
+from ariadne import gql, QueryType, make_executable_schema
 
 
-type_defs = """
+# gql function to validate schema
+type_defs = gql("""
     type Query {
         hello: String!
     }
-"""
+""")
 
+# Create type instance for Query type defined in our schema...
 query = QueryType()
 
 
+# ...and assign our resolver function to its "hello" field.
 @query.field("hello")
-def resolve_hello_world(*_):
-    return "Hello world!"
+def resolve_hello(_, info):
+    request = info.context['request']
+    user_agent = request.headers.get("user-agent", "guest")
+    return f"Hello {user_agent}!"
 
 
 schema = make_executable_schema(type_defs, query)
