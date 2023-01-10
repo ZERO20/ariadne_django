d097268b483c45f579c388d3c043683fb34f705b
ZERO20 <edgargdcv@gmail.com>
2023-01-09 22:03:03 -0600

    feat: added base project

---
+++ README.md
@@ -0,0 +1,18 @@
+# Ariadne Graphql
+
+```Hello world!``` with ```ariadne-django```
+
+- Install the packages
+~~~
+pip install -r requirements.txt
+or
+pipenv install
+~~~
+- Run serverver:
+~~~
+python manage.py runserver
+~~~
+- Access the browser
+~~~
+http://localhost:8000/graphql
+~~~
---
+++ ariadne_tutorial/graphql_schemas/schema.py
@@ -0,0 +1,18 @@
+from ariadne import QueryType, make_executable_schema
+
+
+type_defs = """
+    type Query {
+        hello: String!
+    }
+"""
+
+query = QueryType()
+
+
+@query.field("hello")
+def resolve_hello_world(*_):
+    return "Hello world!"
+
+
+schema = make_executable_schema(type_defs, query)
