b04faa8e4dca261a63573005d4027d188ea30611
ZERO20 <edgargdcv@gmail.com>
2023-01-10 23:05:30 -0600

    ref: binding resolvers

--- ariadne_tutorial/graphql_schemas/schema.py
+++ ariadne_tutorial/graphql_schemas/schema.py
@@ -1,6 +1,6 @@
 import datetime
 
-from ariadne import gql, QueryType, make_executable_schema, ObjectType
+from ariadne import gql, ObjectType, QueryType, make_executable_schema
 
 
 # gql function to validate schema
@@ -10,6 +10,17 @@ type_defs = gql("""
     type Query {
         hello: String!
         time: DateTime!
+        user: User,
+        client: Client,
+    }
+
+    type User {
+        username: String!
+        email: String!
+    }
+
+    type Client{
+        email: String!
     }
 """)
 
@@ -30,4 +41,26 @@ def resolver_current_time(*_):
     return datetime.datetime.now()
 
 
-schema = make_executable_schema(type_defs, query)
+@query.field("user")
+@query.field('client')
+def resolve_user(_, info):
+    return info.context["request"].user
+
+
+# add resolvers for another type
+user = ObjectType("User")
+client = ObjectType("Client")
+
+
+@user.field("username")
+def resolve_username(obj, *_):
+    return f'{obj.first_name} {obj.last_name}'
+
+
+@user.field("email")
+@client.field("email")
+def resolve_email(obj, *_):
+    return f'{obj.email}'
+
+
+schema = make_executable_schema(type_defs, query, user, client)
